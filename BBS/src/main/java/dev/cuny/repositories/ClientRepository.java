2c3dbee9781c62df0798fb5a81d07f0666899360
YoTNT <pikaqiujian@gmail.com>
2020-05-15 16:27:12 -0400

    implement clientService

--- BBS/src/main/java/dev/cuny/repositories/ClientRepository.java
+++ BBS/src/main/java/dev/cuny/repositories/ClientRepository.java
@@ -1,5 +1,7 @@
 package dev.cuny.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
@@ -12,4 +14,5 @@ public interface ClientRepository extends CrudRepository<Client,Integer>
 {
 	Client findByUsername(String username);
 	Client findByUsernameAndPassword(String username, String password);
+	List<Client> findByRole(int role);
 }
--- BBS/src/main/java/dev/cuny/services/ClientService.java
+++ BBS/src/main/java/dev/cuny/services/ClientService.java
@@ -1,9 +1,24 @@
 package dev.cuny.services;
 
+import java.util.List;
+
 import dev.cuny.entities.Client;
 
 public interface ClientService {
 	
-	Client createUser(Client client);
-	Client getUserByUsernameAndPassword(String username, String password);
+	// Create
+	Client createClient(Client client);
+	
+	// Read
+	Client getClientById(int id);
+	Client getClientByUsernameAndPassword(String username, String password);
+	Client getClientByUsername(String username);
+	List<Client> getClientsByRole(int role);
+	List<Client> getAllClients();
+	
+	// Update
+	Client updateClient(Client client);
+	
+	// Delete
+	boolean deleteClient(Client client);
 }
