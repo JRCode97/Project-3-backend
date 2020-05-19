5cb1e072a0ef769479c79b910a4c751992abe4ed
Dylan graham <43048723+DylanGraham140@users.noreply.github.com>
2020-05-19 14:04:11 -0400

    Change jpa to CrudRepository
    CrudRepository

--- BBS/src/main/java/dev/cuny/repositories/ApplicationRepository.java
+++ BBS/src/main/java/dev/cuny/repositories/ApplicationRepository.java
@@ -1,10 +1,10 @@
 package dev.cuny.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import dev.cuny.entities.Application;
 
-public interface ApplicationRepository extends JpaRepository<Application,Integer>{
+public interface ApplicationRepository extends CrudRepository<Application,Integer>{
 	Application findByTitle(String title);
 	
 }
--- BBS/src/main/java/dev/cuny/repositories/LeaderBoardRepository.java
+++ BBS/src/main/java/dev/cuny/repositories/LeaderBoardRepository.java
@@ -3,13 +3,11 @@ package dev.cuny.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import dev.cuny.entities.BugReport;
-import dev.cuny.entities.Client;
 
 @Component
 @Repository
