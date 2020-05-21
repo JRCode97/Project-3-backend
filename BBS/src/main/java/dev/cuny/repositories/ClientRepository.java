23b32ff915a0d7c4badd3574e7090f290c1bcd63
Dylan graham <43048723+DylanGraham140@users.noreply.github.com>
2020-05-21 15:15:26 -0400

    Change Approved to Accepted
    Change Approved to Accepted

--- BBS/src/main/java/dev/cuny/repositories/ClientRepository.java
+++ BBS/src/main/java/dev/cuny/repositories/ClientRepository.java
@@ -17,20 +17,15 @@ public interface ClientRepository extends JpaRepository<Client,Integer>
 	Client findByUsernameAndPassword(String username, String password);
 	List<Client> findByRole(int role);
 	
-	@Query(value="SELECT sum(br.pointValue) from Solution s,BugReport br,Client c where s.br =br.bId  and s.client=c.cId and c.cId =?1 and s.status ='approved'")
+	@Query(value="SELECT sum(br.pointValue) from Solution s,BugReport br,Client c where s.br =br.bId  and s.client=c.cId and c.cId =?1 and s.status ='Accepted'")
 	int getClientPoints(int id);
 	
-	
-	
-	@Query(value ="SELECT c.username from Client c, BugReport br, Solution s where br.bId = s.br and c.cId = s.client and s.status ='approved' group by c.cId order by sum(br.pointValue) desc")
+	@Query(value ="SELECT c.username from Client c, BugReport br, Solution s where br.bId = s.br and c.cId = s.client and s.status ='Accepted' group by c.cId order by sum(br.pointValue) desc")
 	List<String> getLeaderBoardUsernames();
 	
-	@Query(value ="SELECT sum(br.pointValue) from Client c, BugReport br, Solution s where br.bId = s.br and c.cId = s.client and s.status ='approved' group by c.cId order by sum(br.pointValue) desc")
+	@Query(value ="SELECT sum(br.pointValue) from Client c, BugReport br, Solution s where br.bId = s.br and c.cId = s.client and s.status ='Accepted' group by c.cId order by sum(br.pointValue) desc")
 	List<Integer> getLeaderBoardPoints();
-	
 
-	@Query(value ="SELECT sum(br.pointValue), c.username from Client c, BugReport br, Solution s where br.bId = s.br and c.cId = s.client and s.status ='approved' group by c.cId order by sum(br.pointValue) desc")
-	List<Object> getLeaderBoard();
 			
 	
 }
