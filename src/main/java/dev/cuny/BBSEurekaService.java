022d9551ef4859ef239c77a7fb999b861ee3a2b9
Nai Bing Jiang <naibingjiang@gmail.com>
2020-05-21 14:43:32 -0400

    Initiliazed EurekaService Project

---
+++ src/main/java/dev/cuny/BBSEurekaService.java
@@ -0,0 +1,15 @@
+package dev.cuny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BBSEurekaService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BBSEurekaService.class, args);
+	}
+
+}
