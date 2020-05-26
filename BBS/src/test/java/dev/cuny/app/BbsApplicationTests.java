61352ea24f2234d49d9a311ba8f3db1222acc6e0
JamesBrandow <62569945+JamesBrandow@users.noreply.github.com>
2020-05-26 16:11:43 -0400

    Change assertTrue to assertNotNull

--- BBS/src/test/java/dev/cuny/app/BbsApplicationTests.java
+++ BBS/src/test/java/dev/cuny/app/BbsApplicationTests.java
@@ -22,13 +22,13 @@ class BbsApplicationTests {
 	@Test
 	@Order(1)
 	void getClientbyId() {
-		Assertions.assertTrue(cr.findById(1) != null);
+		Assertions.assertNotNull(cr.findById(1) != null);
 	}
 	
 	@Test
 	@Order(2)
 	void getClientByUsername() {
-		Assertions.assertTrue(cr.findByUsername("theRaidMan") != null);
+		Assertions.assertNotNull(cr.findByUsername("theRaidMan") != null);
 		
 	}
 	
@@ -38,7 +38,7 @@ class BbsApplicationTests {
 		String username = "theRaidMan";
 		String password = "password";
 
-		Assertions.assertTrue(cr.findByUsernameAndPassword(username, password) != null);
+		Assertions.assertNotNull(cr.findByUsernameAndPassword(username, password) != null);
 	}
 
 }
